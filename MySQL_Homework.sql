bb405645b58bf8fe2c61a4705c80080d9b806eda
ENGRMike <michael.s.bruins@gmail.com>
2018-04-28 16:23:41 -0400

    progressed to question 7a

--- MySQL_Homework.sql
+++ MySQL_Homework.sql
@@ -17,11 +17,77 @@ SELECT * FROM actor WHERE last_name LIKE "%GEN%";
 SELECT * FROM actor WHERE last_name LIKE "%LI%"
 ORDER BY last_name, first_name ASC;
 
-
 -- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
+SELECT * FROM country 
+WHERE country IN ("Afghanistan", "Bangladesh", "China");
 
+-- 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
+ALTER TABLE actor
+ADD middle_name VARCHAR(64) AFTER first_name;
+SELECT * FROM actor;
+
+-- 3b. You realize that some of these actors have tremendously long last names. Change the data type of the `middle_name` column to `blobs`.
+ALTER TABLE actor
+MODIFY COLUMN middle_name BLOB;
+
+-- 3c. Now delete the `middle_name` column.
+ALTER TABLE actor
+DROP COLUMN middle_name;
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(*) name_count FROM actor GROUP BY last_name;
+
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT last_name, COUNT(*) name_count FROM actor GROUP BY last_name
+HAVING COUNT(*)>1;
+
+-- 4c. Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+
+-- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`. Otherwise, change the first name to `MUCHO GROUCHO`, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO `MUCHO GROUCHO`, HOWEVER! (Hint: update the record using a unique identifier.)
 UPDATE actor
 set first_name = (case
 	WHEN first_name = "HARPO" then "GROUCHO" ELSE "MUCHO GROUCHO"
     END)
 WHERE actor_id=172;
+
+-- 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it? 
+SELECT * FROM information_schema.COLUMNS
+WHERE TABLE_NAME ='address';
+
+-- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+SELECT * FROM staff s
+JOIN address a ON s.address_id = a.address_id;
+
+-- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`. 
+SELECT s.staff_id, s.first_name, s.last_name, SUM(p.amount) August_05_Totals FROM staff s
+JOIN payment p ON s.staff_id = p.staff_id
+WHERE p.payment_date LIKE "2005-08%"
+GROUP BY s.staff_id;
+
+-- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
+SELECT f.title, COUNT(*) actor_count
+FROM film_actor fa
+JOIN film f ON fa.film_id = f.film_id
+GROUP BY actor_id
+ORDER BY actor_count desc;
+
+-- 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
+SELECT f.title, COUNT(*) inventory_count
+FROM inventory i
+JOIN film f ON f.film_id = i.film_id
+WHERE f.title LIKE "Hunchback Impossible";
+
+-- 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
+SELECT first_name, last_name, COUNT(*) total_payment
+FROM customer c
+JOIN payment p on c.customer_id = p.customer_id
+GROUP BY p.amount
+ORDER BY last_name asc;
+
+-- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English. 
+
+
+
